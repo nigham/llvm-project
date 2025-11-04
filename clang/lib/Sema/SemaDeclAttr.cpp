1b9cdfdf0fd122b2f838c7f929c0a5dec1deadcb
Daniel Thornburgh <dthorn@google.com>
2025-11-03 16:48:55 -0800

    Clarify and correct docs

--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -6927,7 +6927,6 @@ static void handleModularFormat(Sema &S, Decl *D, const ParsedAttr &AL) {
   llvm::sort(Aspects);
   Aspects.erase(llvm::unique(Aspects), Aspects.end());
 
-  // TODO: Type checking on identifier
   D->addAttr(::new (S.Context) ModularFormatAttr(
       S.Context, AL, AL.getArgAsIdent(0)->getIdentifierInfo(), ImplName,
       Aspects.data(), Aspects.size()));
